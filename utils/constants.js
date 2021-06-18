3f5afb2fbcebeb4185a4ad6c79b58948c2e17e7b
Oleg Zharov <levineye@gmail.com>
2021-06-18 19:17:25 +0300

    feat: добавить константу mimetypes

---
+++ utils/constants.js
@@ -0,0 +1,14 @@
+const mimeTypes = {
+  '.html': 'text/html',
+  '.css': 'text/css',
+  '.js': 'application/javascript',
+  '.json': 'application/json',
+  '.jpg': 'image/jpeg',
+  '.svg': 'image/svg+xml',
+  '.png': 'image/png',
+  '.ico': 'image/x-icon',
+  '.woff2': 'font/woff2',
+  '.woff': 'font/woff',
+};
+
+module.exports = { mimeTypes };
