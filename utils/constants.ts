0a4f26228669c5e83c96af0e780286a55e7cb474
levineye <levineye@gmail.com>
2021-08-30 23:36:03 +0300

    feat: переписать константы на ts

---
+++ utils/constants.ts
@@ -0,0 +1,24 @@
+import { IHttpMethods } from "./interfaces";
+
+export const MIME_TYPES: { [ext: string]: string } = {
+  ".html": "text/html",
+  ".css": "text/css",
+  ".js": "application/javascript",
+  ".json": "application/json",
+  ".jpg": "image/jpeg",
+  ".svg": "image/svg+xml",
+  ".png": "image/png",
+  ".ico": "image/x-icon",
+  ".woff2": "font/woff2",
+  ".woff": "font/woff",
+};
+
+export const HTTP_METHODS: IHttpMethods = {
+  OPTIONS: "OPTIONS",
+  HEAD: "HEAD",
+  GET: "GET",
+  POST: "POST",
+  PATCH: "PATCH",
+  PUT: "PUT",
+  DELETE: "DELETE",
+};
