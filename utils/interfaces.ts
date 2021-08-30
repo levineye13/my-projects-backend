32f6057dbeba7bfae75c25df9ad6dda4d2561102
levineye <levineye@gmail.com>
2021-08-30 23:36:17 +0300

    feat: добавить файл с интерфейсами

---
+++ utils/interfaces.ts
@@ -0,0 +1,9 @@
+export interface IHttpMethods {
+  readonly OPTIONS: string;
+  readonly HEAD: string;
+  readonly GET: string;
+  readonly POST: string;
+  readonly PATCH: string;
+  readonly PUT: string;
+  readonly DELETE: string;
+}
