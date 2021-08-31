dcc7d750581cd8ff0fb45b6e668ba5a19221464c
levineye <levineye@gmail.com>
2021-08-31 15:56:15 +0300

    fix: убрать лишнюю функцию

--- routes/statics.ts
+++ routes/statics.ts
@@ -1,65 +1,49 @@
-import fsPromises from "fs/promises";
-import path from "path";
+import fsPromises from 'fs/promises';
+import path from 'path';
 
-import { IncomingMessage, ServerResponse } from "../utils/types";
-import { MIME_TYPES } from "../utils/constants";
+import { IncomingMessage, ServerResponse } from '../utils/types';
+import { MIME_TYPES } from '../utils/constants';
 
 const getStatic = (
   req: IncomingMessage,
   res: ServerResponse
-): { [functionName: string]: Function } | void => {
-  const { url } = req;
+): { getBinaryFile: Function; getStaticFile: Function } => {
+  const { url = '/' } = req;
 
-  if (typeof url === "undefined") {
-    return;
-  }
-
-  let filePath: string = path.join(__dirname, "..", "build", url);
+  let filePath: string = path.join(__dirname, '..', 'build', url);
   const ext: string = path.extname(url);
-  let contentType: string = MIME_TYPES[ext] || "text/plain";
+  let contentType: string = MIME_TYPES[ext] || 'text/plain';
 
-  if (url === "/") {
-    filePath += "index.html";
-    contentType = "text/html";
+  if (url === '/') {
+    filePath += 'index.html';
+    contentType = 'text/html';
   }
 
-  let data: string = "";
+  let data: string = '';
 
   const getBinaryFile = async (): Promise<void> => {
     try {
-      data = await fsPromises.readFile(filePath, { encoding: "binary" });
+      data = await fsPromises.readFile(filePath, { encoding: 'binary' });
     } catch (err) {
       console.error(err);
     }
 
-    res.writeHead(200, { "Content-Type": contentType }).end(data, "binary");
+    res.writeHead(200, { 'Content-Type': contentType }).end(data, 'binary');
   };
 
   const getStaticFile = async (): Promise<void> => {
     try {
       data = await fsPromises.readFile(filePath, {
-        encoding: "utf-8",
+        encoding: 'utf-8',
       });
     } catch (err) {
       console.error(err);
     }
 
-    res.writeHead(200, { "Content-Type": contentType }).end(data, "utf-8");
+    res.writeHead(200, { 'Content-Type': contentType }).end(data, 'utf-8');
   };
 
-  const getMainPage = async (): Promise<void> => {
-    try {
-      data = await fsPromises.readFile(filePath, {
-        encoding: "utf-8",
-      });
-    } catch (err) {
-      console.error(err);
-    }
-
-    res.writeHead(200, { "Content-Type": contentType }).end(data, "utf-8");
-  };
-
-  return { getMainPage, getStaticFile, getBinaryFile };
+  return { getStaticFile, getBinaryFile };
 };
 
 export { getStatic };
